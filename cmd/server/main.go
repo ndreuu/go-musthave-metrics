c8c8a87492ddff068c701e2bceab4c0b70fa92c3
Олейников Андрей Павлович <a.p.oleynikov@tbank.ru>
2026-05-27 10:47:06 +0300

    iter6

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,15 +5,18 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"go-musthave-metrics/internal/handler"
 	"go-musthave-metrics/internal/repository"
 	"go-musthave-metrics/internal/service"
+	"go.uber.org/zap"
 )
 
 var (
 	flagRunAddr string
+	logger      *zap.Logger
 )
 
 func parseFlags() {
@@ -25,8 +28,33 @@ func parseFlags() {
 	}
 }
 
+func loggingMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		start := time.Now()
+
+		c.Next()
+
+		duration := time.Since(start)
+
+		logger.Info("request completed",
+			zap.String("method", c.Request.Method),
+			zap.String("uri", c.Request.RequestURI),
+			zap.Duration("duration", duration),
+			zap.Int("status", c.Writer.Status()),
+			zap.Int("size", c.Writer.Size()),
+		)
+	}
+}
+
 func main() {
 	parseFlags()
+
+	var err error
+	logger, err = zap.NewProduction()
+	if err != nil {
+		log.Fatalf("Failed to create logger: %v", err)
+	}
+	defer logger.Sync()
 
 	storage := repository.NewMemStorage()
 
@@ -35,6 +63,8 @@ func main() {
 	metricsHandler := handler.NewMetricsHandler(metricsService)
 
 	r := gin.Default()
+
+	r.Use(loggingMiddleware())
 
 	r.POST("/update/:type/:name/:value", metricsHandler.UpdateMetricHandler)
 	r.GET("/value/:type/:name", metricsHandler.GetMetricHandler)
